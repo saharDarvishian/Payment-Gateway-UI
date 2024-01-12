7fd2aa55b07bf63b1359d40850e4e2f02c4a7c4d
saharDarvishian <darvishiansahar@gmail.com>
2024-01-12 20:14:08 +0330

    add hamburger-menu & dropdown

--- js/script.js
+++ js/script.js
@@ -1,36 +1,47 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const crossIcon = document.querySelector('.cross-icon');
-const showMoreMenu = document.querySelector('#show-more-menu');
-const showProductMenu = document.querySelector('#show-product-menu');
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
 
-hamburgerIcon.addEventListener('click', function () {
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const crossIcon = document.querySelector(".cross-icon");
+const showMoreMenu = document.querySelector("#show-more-menu");
+const showProductMenu = document.querySelector("#show-product-menu");
+
+hamburgerIcon.addEventListener("click", function () {
   console.log(hamburgerMenu.style.transform);
-  hamburgerMenu.classList.add('show-hamburger-menu');
+  hamburgerMenu.classList.add("show-hamburger-menu");
 });
 
-crossIcon.addEventListener('click', function () {
-  hamburgerMenu.classList.remove('show-hamburger-menu');
+crossIcon.addEventListener("click", function () {
+  hamburgerMenu.classList.remove("show-hamburger-menu");
 });
 
-showMoreMenu.addEventListener('mouseenter', function (e) {
+showMoreMenu.addEventListener("mouseenter", function (e) {
   const ul = e.target.childNodes[5];
-  ul.style.animation = 'showUp 0.35s 1';
-  ul.style.display = 'block';
+  console.log(e.target.childNodes);
+  ul.style.animation = "showUp 0.35s 1";
+  ul.style.display = "block";
 });
 
-showMoreMenu.addEventListener('mouseleave', function (e) {
+showMoreMenu.addEventListener("mouseleave", function (e) {
   const ul = e.target.childNodes[5];
-  ul.style.display = 'none';
+  ul.style.display = "none";
 });
 
-showProductMenu.addEventListener('mouseenter', function (e) {
+showProductMenu.addEventListener("mouseenter", function (e) {
   const div = e.target.childNodes[5];
-  div.style.animation = 'showUp 0.35s 1';
-  div.style.display = 'flex';
+  console.log(e.target.childNodes);
+  div.style.animation = "showUp 0.35s 1";
+  div.style.display = "flex";
 });
 
-showProductMenu.addEventListener('mouseleave', function (e) {
+showProductMenu.addEventListener("mouseleave", function (e) {
   const div = e.target.childNodes[5];
-  div.style.display = 'none';
+  div.style.display = "none";
 });
